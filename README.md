2f80639f969667039257713cd183251a465cb726
Makoto Ishida <70257274+makotoishida@users.noreply.github.com>
2022-04-25 23:56:50 -1000

    Add Deploy scripts for GitHub Pages.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # tspractice10-shopping-list
+
+## Live DEMO
+
+https://makotoishida.github.io/tspractice10-shopping-list/
+
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/tspractice10-shopping-list/',
   server: {
     host: '0.0.0.0',
   },
