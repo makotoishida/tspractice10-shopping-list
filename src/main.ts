2ba8c8ef8ee10b43cf706111c8624d7cd023c040
Makoto Ishida <70257274+makotoishida@users.noreply.github.com>
2022-04-25 15:30:32 -1000

    Initial styling of main page.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import './style.css'
+import "normalize.css";
+import "./style.css";
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const app = document.querySelector<HTMLDivElement>("#app")!;
 
 app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+  <div>TEST</div>
+
+`;
